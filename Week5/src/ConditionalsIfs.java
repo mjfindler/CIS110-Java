35b5ba5b7d645555d87f25393e70f9b4e0c9f895
mjfindler <156967314+mjfindler@users.noreply.github.com>
2025-10-02 14:35:23 -0700

    conditional comparison
    ask students why Switch is better?
         -- easier to read
    Ask why IF is better?
         -- doesn't need straight scale (85% is A)

--- Week5/src/ConditionalsIfs.java
+++ Week5/src/ConditionalsIfs.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 
 public class ConditionalsIfs {
+    public static String getGrade(int grade) {
+        /**
+         * Notice that this does not use {} and is still readable!
+         */
+        if (grade < 59)         return "You got an F.";
+        else if (grade < 69)    return "You got a D.";
+        else if (grade < 79)    return "You got a C.";
+        else if (grade < 89)    return "You got a B.";
+        else                    return "You got an A.";
+    }
+
     public static void main(String[] args) {
         System.out.print("Please enter your grade round to the nearest whole number: ");
         Scanner keyboard = new Scanner(System.in);
         int grade = keyboard.nextInt();
-        if (grade < 59){
-            System.out.print("You got an F.");}
-        else if (grade < 69){
-            System.out.print("You got a D.");}
-        else if (grade < 79){
-            System.out.print("You got a C.");}
-        else if (grade < 89){
-            System.out.print("You got a B.");}
-        else{
-            System.out.print("You got an A.");
-        }
+        System.out.println(getGrade(grade));
     }
 }
--- Week5/src/ConditionalsSwitch.java
+++ Week5/src/ConditionalsSwitch.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 
 public class ConditionalsSwitch {
+    public static String getGrade(int grade) {
+        /**
+         * Note that there are no break statements!
+         * Note that the case statements are ONE line each!
+         */
+        grade /= 10; // get the ten's digit
+        String str = "";
+        switch (grade) {
+            case 9, 10:            return "You got an A.";
+            case 8:                return "You got an B.";
+            case 7:                return "You got an C.";
+            case 6:                return "You got an D.";
+            default:               return "You got an F.";
+        }
+    }
+
     public static void main(String[] args) {
         System.out.print("Please enter your grade round to the nearest whole number: ");
         Scanner keyboard = new Scanner(System.in);
+
         int grade = keyboard.nextInt();
-        grade /= 10; // get the ten's digit
-        switch (grade) {
-            case 9, 10:
-                System.out.print("You got an A.");
-                break;
-            case 8:
-                System.out.print("You got an B.");
-                break;
-            case 7:
-                System.out.print("You got an C.");
-                break;
-            case 6:
-                System.out.print("You got an D.");
-                break;
-            default:
-                System.out.print("You got an F.");
-                break;
-        }
+        System.out.println(getGrade(grade)); // Does one thing! Returns the grade!
     }
 }
